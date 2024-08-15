639cd700c9d7070b5ba0c57e5124de526935534f
Nigel Dube <sangweni1820@gmail.com>
2024-08-15 11:48:26 -0600

    Adding recent projects

--- js/script.js
+++ js/script.js
@@ -1,36 +1,44 @@
-const header = document.querySelector("header");
+const header=document.querySelector("header");
+
 window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 100);
-});
+		header.classList.toggle ("sticky", window.scrollY > 100);
+	});
 
-let menu = document.querySelector('#menu-icon');
-let navlist = document.querySelector('.navlist');
+let menu=document.querySelector('#menu-icon');
+let navlist=document.querySelector('.navlist');
 
-menu.onclick = () => {
+menu.onclick=()=> {
 	menu.classList.toggle('bx-x');
 	navlist.classList.toggle('open');
-};
+}
 
-window.onscroll = () => {
+;
+
+window.onscroll=()=> {
 	menu.classList.remove('bx-x');
 	navlist.classList.remove('open');
-};
+}
+
+;
 
 
-
-let slideIndex = 0;
+let slideIndex=0;
 showSlides();
 
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+	let i;
+	let slides=document.getElementsByClassName("mySlides");
+
+	for (i=0; i < slides.length; i++) {
+		slides[i].style.display="none";
+	}
+
+	slideIndex++;
+
+	if (slideIndex > slides.length) {
+		slideIndex=1
+	}
+
+	slides[slideIndex-1].style.display="block";
+	setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-
-
