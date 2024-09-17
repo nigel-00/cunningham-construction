0e17971cf2b40f023d7ff34cbfad2e40baba9fe1
Nigel Dube <sangweni1820@gmail.com>
2024-09-16 21:16:13 -0600

    Add videos

--- js/script.js
+++ js/script.js
@@ -17,28 +17,14 @@ menu.onclick=()=> {
 window.onscroll=()=> {
 	menu.classList.remove('bx-x');
 	navlist.classList.remove('open');
+};
+
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
 }
 
-;
-
-
-let slideIndex=0;
-showSlides();
-
-function showSlides() {
-	let i;
-	let slides=document.getElementsByClassName("mySlides");
-
-	for (i=0; i < slides.length; i++) {
-		slides[i].style.display="none";
-	}
-
-	slideIndex++;
-
-	if (slideIndex > slides.length) {
-		slideIndex=1
-	}
-
-	slides[slideIndex-1].style.display="block";
-	setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
